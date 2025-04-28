98de166f6cb5984fb016bb8dcfd9294237d6b654
Wenard Santos <wenardken57@gmail.com>
2025-04-28 19:44:07 +0800

    feat: add css style-loader and css-loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,6 +25,10 @@ module.exports = {
       {
         test: /\.html/i,
         use: "html-loader",
+      },
+      {
+        test: /\.css/i,
+        use: ["style-loader", "css-loader"],
       }
     ]
   }
